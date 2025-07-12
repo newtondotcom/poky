4c0ac9ffdf3cfdd402bc005908840a1834838d90
newtondotcom <asphalt9fr@gmail.com>
2025-07-12 23:05:05 +0200

    feat(back): add more datas on users

--- apps/server/src/db/schema/auth.ts
+++ apps/server/src/db/schema/auth.ts
@@ -3,6 +3,7 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 export const user = sqliteTable("user", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
+  username : text("username"),
   email: text("email").notNull().unique(),
   emailVerified: integer("email_verified", { mode: "boolean" }).notNull(),
   image: text("image"),
--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -17,7 +17,38 @@ export const auth = betterAuth({
               clientId: process.env.CHURROS_CLIENT_ID || "",
               clientSecret: process.env.CHURROS_CLIENT_SECRET || "",
               authorizationUrl : process.env.CHURROS_AUTHORIZATION_URL || "",
-              tokenUrl : process.env.CHURROS_TOKEN_URL || ""
+              tokenUrl : process.env.CHURROS_TOKEN_URL || "",
+              scopes : ['openid', 'profile', 'preferred_username',"email","churros:profile"],
+              async getUserInfo(tokens) {
+                const userInfoUrl = process.env.CHURROS_USER_INFO;
+                if (!userInfoUrl) {
+                  throw new Error("CHURROS_USER_INFO environment variable is not set");
+                }
+
+                const response = await fetch(userInfoUrl, {
+                  headers: {
+                    "Authorization": `Bearer ${tokens.accessToken}`,
+                    "Content-Type": "application/json",
+                  },
+                });
+
+                if (!response.ok) {
+                  throw new Error(`Failed to fetch user info: ${response.status} ${response.statusText}`);
+                }
+
+                const userInfo = await response.json();
+
+                return {
+                  id: userInfo.sub || "", // could be uid but sub will be used for anonuymsation
+                  name: userInfo.fullName ||"",
+                  username: userInfo.uid || "",
+                  image: userInfo.pictureURL || null,
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                  email : userInfo.email,
+                  emailVerified : userInfo.email_verified
+                };
+              },
           }, 
       ] 
   }) ],
