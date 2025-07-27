e8a9bada5e90624185e0970965d02e1d681cdee4
newtondotcom <asphalt9fr@gmail.com>
2025-07-27 17:18:04 +0200

    feat(front): add a `splitLink` for query and suscription to client

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -1,7 +1,7 @@
 import { toast } from "@pheralb/toast";
 import type { AppRouter } from "../../../server/src/routers";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
-import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import { createTRPCClient, httpBatchLink, httpSubscriptionLink, loggerLink, splitLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 
 export const queryClient = new QueryClient({
@@ -22,14 +22,28 @@ export const queryClient = new QueryClient({
 
 export const trpcClient = createTRPCClient<AppRouter>({
   links: [
-    httpBatchLink({
-      url: `${import.meta.env.VITE_SERVER_URL}/trpc`,
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          credentials: "include",
-        });
-      },
+    loggerLink(),
+    splitLink({
+      // uses the httpSubscriptionLink for subscriptions
+      condition: (op) => op.type === 'subscription',
+      true: httpSubscriptionLink({
+        url: `${import.meta.env.VITE_SERVER_URL}/trpc`,
+        eventSourceOptions() {
+          return {
+            withCredentials: true,
+          };
+        },
+      }),
+      // uses the httpBatchLink for simple fetch query
+      false: httpBatchLink({
+        url: `${import.meta.env.VITE_SERVER_URL}/trpc`,
+        fetch(url, options) {
+          return fetch(url, {
+            ...options,
+            credentials: "include",
+          });
+        },
+      }),
     }),
   ],
 });
