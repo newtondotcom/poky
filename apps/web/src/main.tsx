15c3c35bd1915521ec8cb86a0639d467d114ec4e
newtondotcom <asphalt9fr@gmail.com>
2025-10-20 11:22:34 +0200

    fix: generated proto code for client

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,8 +3,33 @@ import ReactDOM from "react-dom/client";
 import Loader from "./components/loader";
 import { routeTree } from "./routeTree.gen";
 
-  import { QueryClientProvider } from "@tanstack/react-query";
-  import { queryClient, trpc } from "./utils/trpc";
+const finalTransport = createConnectTransport({
+  baseUrl: "https://demo.connectrpc.com",
+  interceptors: [
+    (next) => (request) => {
+      const token = "test";
+      request.header.append("Authorization", token);
+      // Add your headers here
+      return next(request);
+    },
+  ],
+});
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast.error({
+        text :error.message,
+        action: {
+          content: "retry",
+          onClick: () => {
+            queryClient.invalidateQueries();
+          },
+        },
+      });
+    },
+  }),
+})
 
 const router = createRouter({
   routeTree,
