cc02d376272227cba25cee2d7c08ad1c46af2257
newtondotcom <asphalt9fr@gmail.com>
2025-10-20 16:34:11 +0200

    chore: fix db url

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-export const client = postgres(process.env.LOCAL_DATABASE_URL as string, {
+export const client = postgres(process.env.DATABASE_URL as string, {
   prepare: false,
 });
 export const db = drizzle(client);
