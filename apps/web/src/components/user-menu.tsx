93cb44557821234f64c4dc287bf48f7a91a74206
newtondotcom <asphalt9fr@gmail.com>
2025-10-21 19:21:09 +0200

    style: centering auth button

--- apps/web/src/components/user-menu.tsx
+++ apps/web/src/components/user-menu.tsx
@@ -11,7 +11,7 @@ export default function UserMenu() {
 
   if (!error) {
     return (
-  <>
+  <div className="w-full flex flex-col justify-center items-center">
   < DynamicText />
   <div className="flex flex-col px-8 gap-2 max-w-md bg-black/20 backdrop-blur-sm border border-white/50 rounded-lg shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] p-2 text-white relative before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none">
         <h2 className="text-xl font-semibold text-white/90">Authentification</h2>
@@ -21,7 +21,7 @@ export default function UserMenu() {
           with UnAuth
     </button>
   </div>
-  </>
+  </div>
     );
   }
 }
