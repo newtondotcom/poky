c4993d72c6c73af35b1b8ef8810a6564cd12e61b
newtondotcom <asphalt9fr@gmail.com>
2025-10-22 14:22:39 +0200

    fix: working building image

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,23 +6,34 @@ import { cors as connectCors } from "@connectrpc/connect";
 import { authInterceptor } from "@/rpc/interceptor";
 import logger from "./lib/logger";
 
-const server = fastify();
-await server.register(fastifyCors, {
-  // Reflects the request origin. This should only be used for development.
-  // Production should explicitly specify an origin
-  origin: true,
-  methods: [...connectCors.allowedMethods],
-  allowedHeaders: [...connectCors.allowedHeaders, "Authorization"],
-  exposedHeaders: [...connectCors.exposedHeaders],
+async function startServer() {
+  const server = fastify();
+  
+  await server.register(fastifyCors, {
+    // Reflects the request origin. This should only be used for development.
+    // Production should explicitly specify an origin
+    origin: true,
+    methods: [...connectCors.allowedMethods],
+    allowedHeaders: [...connectCors.allowedHeaders, "Authorization"],
+    exposedHeaders: [...connectCors.exposedHeaders],
+  });
+  
+  await server.register(fastifyConnectPlugin, {
+    routes,
+    interceptors: [authInterceptor],
+  });
+  
+  server.get("/", (_, reply) => {
+    reply.type("text/plain");
+    reply.code(200);
+    reply.send("Hello World!");
+  });
+  
+  await server.listen({ host: "localhost", port: 8080 });
+  logger.debug("server is listening at", server.addresses());
+}
+
+startServer().catch((error) => {
+  logger.error("Failed to start server:", error);
+  process.exit(1);
 });
-await server.register(fastifyConnectPlugin, {
-  routes,
-  interceptors: [authInterceptor],
-});
-server.get("/", (_, reply) => {
-  reply.type("text/plain");
-  reply.code(200);
-  reply.send("Hello World!");
-});
-await server.listen({ host: "localhost", port: 8080 });
-logger.debug("server is listening at", server.addresses());
